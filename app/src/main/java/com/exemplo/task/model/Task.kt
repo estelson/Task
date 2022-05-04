1df5bf35714a3d9504e8d9b19864f0414e495015
Estelson Medeiros Pereira <estelson@gmail.com>
2022-05-03 23:51:08 -0300

    #6
    - Criar ItemAdapter
    - Criar objeto Task
    - Criar TaskAdapter

---
+++ app/src/main/java/com/exemplo/task/model/Task.kt
@@ -0,0 +1,20 @@
+package com.exemplo.task.model
+
+import android.os.Parcelable
+import com.exemplo.task.helper.FirebaseHelper
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Task(
+
+    var id: String = "",
+    var title: String = "",
+    var status: Int = 0
+
+): Parcelable {
+
+    init {
+        this.id = FirebaseHelper.getDatabase().push().key ?: ""
+    }
+
+}
